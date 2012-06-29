cecd13f18302d0739765fb8c75294c962fd5c9e5
x11106506.student.ncirl.ie <desmond.harrold@student.ncirl.ie>
2012-06-29 20:04:20 +0100

    PlanApp Vers 1.0
    First working skeleton - permission class and blob management

---
+++ CS - WIP2906/Plan1/Plan1/Controllers/HelloWorldController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Plan1.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //
+        // GET: /HelloWorld/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // Get: /HelloWorld/Welcome/
+
+        public ActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewBag.Message = "Hello " + name;
+            ViewBag.NumTimes = numTimes;
+
+            return View();
+        }
+
+    }
+}
---
+++ CS - WIP2906/Plan1/Plan1/Controllers/PermissionController.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Plan1.Models;
+
+namespace Plan1.Controllers
+{ 
+    public class PermissionController : Controller
+    {
+        private PermissionDBContext db = new PermissionDBContext();
+
+        //
+        // GET: /Permission/
+
+        public ViewResult Index()
+        {
+            return View(db.Permissions.Take(8).ToList());
+        }
+
+        //
+        // GET: /Permission/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Permission permission = db.Permissions.Find(id);
+            if (permission == null)
+            {
+                return HttpNotFound();
+            }
+            return View(permission);
+        }
+
+        //
+        // GET: /Permission/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        } 
+
+        //
+        // POST: /Permission/Create
+
+        [HttpPost]
+        public ActionResult Create(Permission permission)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Permissions.Add(permission);
+                db.SaveChanges();
+                return RedirectToAction("Index");  
+            }
+
+            return View(permission);
+        }
+        
+        //
+        // GET: /Permission/Edit/5
+ 
+        public ActionResult Edit(int id = 0)
+        {
+            Permission permission = db.Permissions.Find(id);
+            if (permission == null)
+            {
+                return HttpNotFound();
+            }
+            return View(permission);
+        }
+
+        //
+        // POST: /Permission/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Permission permission)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(permission).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(permission);
+        }
+
+        //
+        // GET: /Permission/Delete/5
+ 
+        public ActionResult Delete(int id = 0)
+        {
+            Permission permission = db.Permissions.Find(id);
+            if (permission == null)
+            {
+                return HttpNotFound();
+            }
+            return View(permission);
+        }
+
+        //
+        // POST: /Permission/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id = 0)
+        {            
+            Permission permission = db.Permissions.Find(id);
+            if (permission == null)
+            {
+                return HttpNotFound();
+            }
+            db.Permissions.Remove(permission);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+
+        public ActionResult SearchIndex( string planAddr, string searchString)
+        {   
+            var AddrLst = new List<string>();
+
+            var AddrQry = from d in db.Permissions
+                   orderby d.postalAddr
+                   select d.postalAddr;
+            AddrLst.AddRange(AddrQry.Distinct());
+            ViewBag.planAddr = new SelectList(AddrLst);
+ 
+            var permissions = from m in db.Permissions
+                         select m;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                permissions = permissions.Where(s => s.appName.Contains(searchString)).Take(3);
+            }
+
+            if (string.IsNullOrEmpty(planAddr))
+                return View(permissions.Take(3));
+            else
+            {
+                return View(permissions.Where(x => x.postalAddr == planAddr));
+            }
+
+        }
+    }
+}
