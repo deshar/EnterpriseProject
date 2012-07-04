f13360eaebb78a4fcf22c5ab64c2bf57bc2f9c51
x11106506.student.ncirl.ie <desmond.harrold@student.ncirl.ie>
2012-07-04 09:59:05 +0100

    PlanApp v1.3 with Unit Testsr
    The current tests fail and are wip

---
+++ CS - WIP0207-mail-unitt/Plan1/Plan1/Models/DHMailResult.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Plan1.Models
+{
+    public class DHMailResult
+    {
+        public string id { get; set; }
+
+        public string errMessage { get; set; }
+
+        public string superV { get; set; }
+        
+    }
+}
