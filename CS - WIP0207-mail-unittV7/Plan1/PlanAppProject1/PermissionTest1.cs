189a9a3ac36529bfcc86a48f73bae86c9b3568ce
x11106506.student.ncirl.ie <desmond.harrold@student.ncirl.ie>
2012-07-13 17:21:12 +0100

    PlanApp V1.8 with Webmail replaced by Net.Mail

---
+++ CS - WIP0207-mail-unittV7/Plan1/PlanAppProject1/PermissionTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PlanAppProject1
+{
+    [TestClass]
+    public class PermissionTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ CS - WIP0207-mail-unittV7/Plan1/TestPlan1/Models/MocPermissionRepository.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Plan1.Models;
+
+namespace TestPlan1.Models
+{
+    class InMemoryPermissionRepository : Plan1.Models.IPermissionRepository
+    {
+        private List<Permission> _db = new List<Permission>();
+
+        public Exception ExceptionToThrow { get; set; }
+        //public List<Permission> Items { get; set; }
+
+        public void SaveChanges(Permission PermissionToUpdate)
+        {
+
+            foreach (Permission Permission in _db)
+            {
+                if (Permission.Id == PermissionToUpdate.Id)
+                {
+                    _db.Remove(Permission);
+                    _db.Add(PermissionToUpdate);
+                    break;
+                }
+            }
+        }
+
+        public void Add(Permission PermissionToAdd)
+        {
+            _db.Add(PermissionToAdd);
+        }
+
+        public Permission GetPermissionByID(int id)
+        {
+            return _db.FirstOrDefault(d => d.Id == id);
+        }
+
+        public void CreateNewPermission(Permission PermissionToCreate)
+        {
+            if (ExceptionToThrow != null)
+                throw ExceptionToThrow;
+
+            _db.Add(PermissionToCreate);
+            // return PermissionToCreate;
+        }
+
+        public int SaveChanges()
+        {
+            return 1;
+        }
+
+        public IEnumerable<Permission> GetAllPermissions()
+        {
+            return _db.ToList();
+        }
+
+
+        public void DeletePermission(int id)
+        {
+            _db.Remove(GetPermissionByID(id));
+        }
+
+    }
+} 
+
